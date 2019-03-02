0b661fef474b6d44ffda02bb6dc472b0026dec86
Gordon Young <gjyoung1974@gmail.com>
2019-03-02 12:03:39 -0700

    Update vgs_iot_demo.ino

--- vgs_iot_demo.ino
+++ vgs_iot_demo.ino
@@ -7,68 +7,92 @@
  */
 
 #include <Arduino.h>
-// #include <WiFiMulti.h> //esp32WiFi
-#include <ESP8266WiFi.h> //esp8266WiFi
+#include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
 
-//#include <HTTPClient.h> //ESP32HttpClient
-#include <ESP8266HTTPClient.h>//esp8266 HttpClient
+// The serial connection to the GPS module
+SoftwareSerial ss(12, 13);
 
 //SDA SSD1306 Display
 #include <SSD1306Wire.h>
 #include <SSD1306.h>
 
-#include <SoftwareSerial.h>
+// #include <SoftwareSerial.h>
 
 #define USE_SERIAL Serial //Serial for Debugging
 
-#define SDA_PIN D3//SDA: GPIO21 -> for Esp32
-#define SCL_PIN D5//SCL: GPIO22 -> for Esp32
+// setup the OLED display
+#define SDA_PIN D3
+#define SCL_PIN D5
 #define SSD_ADDRESS 0x3c
-byte gpsData;
 
-// The serial connection to the GPS module
-SoftwareSerial ss(12, 13);
+//configure WiFi
+#ifndef STASSID
+#define STASSID "yourssid"
+#define STAPSK  "YourWifiPassword"
+#endif
+
+const char* ssid = STASSID;
+const char* password = STAPSK;
+
+String sGPS="";
 
 SSD1306  display(SSD_ADDRESS, SDA_PIN, SCL_PIN);
 
-//WiFiMulti wifiMulti; //get a wifi client (esp32)
-ESP8266WiFiMulti wifiMulti; //get a wifi client (esp8266)
+ESP8266WiFiMulti wifiMulti;
 
-char PostData[] = "{\"GPS\": \"334484N1120740WT1526864344\", \"TEMP\": \"48.2C\"}"; // your JSON payload
+// Our JSON payload
+char PostData[1024]; // "{\"GPS\": \"334484N1120740WT1526864344\", \"TEMP\": \"48.2C\"}";
 
 void setup() {
+
+      //8266's serial debug port
+      USE_SERIAL.begin(9600);
+      USE_SERIAL.println();
+      USE_SERIAL.println();
+      USE_SERIAL.println();
+
+      //Sofware Serial Bus for serial devices
+      ss.begin(9600);
 
       display.init();
       display.flipScreenVertically();
       display.drawString(0, 0, "VGS IOT Demo");
       display.display();
 
-      USE_SERIAL.begin(9600);
-      USE_SERIAL.println();
-      USE_SERIAL.println();
-      USE_SERIAL.println();
+      Serial.print("connecting to ");
+      Serial.println(ssid);
+      WiFi.mode(WIFI_STA);
+      WiFi.begin(ssid, password);
       
-      ss.begin(9600);//start sofware serial to speak to the GPS
-
-    for(uint8_t t = 4; t > 0; t--) {
-        USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
-        USE_SERIAL.flush();
-        delay(1000);
-    }
-
-    wifiMulti.addAP("some-ssid-here", "SomePassword");
+      while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+      }
+  
+      Serial.println("");
+      Serial.println("WiFi connected");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      display.clear();
+      display.flipScreenVertically();
+      display.setFont(ArialMT_Plain_10);
+      display.drawString(0, 0, "Wifi Connected");
+      display.drawString(0, 10, "IP address: "  + ipToString(WiFi.localIP()));
+      display.display();
+      
 }
 
 void loop() {
-    // wait for GPS and WiFi connections
-    while (ss.available() > 0){
-      gpsData = ss.read();
+    
+    while (true){
       
       if((wifiMulti.run() == WL_CONNECTED)) {
 
-
         HTTPClient http;
+        sGPS="";
 
         USE_SERIAL.print("[HTTP] begin...\n");
 
@@ -76,57 +100,72 @@ void loop() {
         http.addHeader("Content-Type", "application/json");
         
         // configure server and url
-        http.begin("https://tntsicixns3.SANDBOX.verygoodproxy.com/post","4ad6efaec82ca1d295bf93017e7bde5ac8881eb4"); //<< Http client requires "cert pinning"
+        http.begin("https://yourtenantid.SANDBOX.verygoodproxy.com/post","1f7f92d283e33c34cac6e716837a1dd3650e60f0"); //<< Http client requires "cert pinning"
         http.addHeader("Content-type", "application/json"); // <<Allows us to filter on just the desired data structures vs all streams
 
-        //for proxy authentication:
-        //http.begin("http://US2dihmmMZD8BGsQj2yKgjZk:6e478e95-52ed-4c3b-9493-3aefa7f9137a@tntlvnzzqsz.SANDBOX.verygoodproxy.com:8080");
-        /*
-          // or
-          http.begin("http://tntlvnzzqsz.SANDBOX.verygoodproxy.com:8080");
-          http.setAuthorization("<some user>", "<some password>");
-
-        */
-
         USE_SERIAL.print("[HTTP] POST...\n");
+        USE_SERIAL.println("gps data");
+        
+          while (ss.available() > 0){
+            // get the char data byte from the GPS
+            char gpsData = ss.read();
+            sGPS += String(gpsData);//build a string
+          }
+          
+        USE_SERIAL.println(sGPS);
+
+        //display some tokenz
+        display.flipScreenVertically();
+        display.setFont(ArialMT_Plain_10);
+        display.drawString(0, 40, sGPS);
+        display.display();
+        
         // start connection and send HTTP header
+        String("{\"GPS\": \"" + sGPS + "\", \"TEMP\": \"48.2C\"}").toCharArray(PostData,1024);
+        
         int httpCode = http.POST(PostData);
 
         // httpCode will be negative on error
         if(httpCode > 0) {
+          
             // HTTP header has been send and Server response header has been handled
             USE_SERIAL.printf("[HTTP] POST... code: %d\n", httpCode);
                  
-                 display.clear();
-                //text display tests
-                display.flipScreenVertically();
-                display.setFont(ArialMT_Plain_16);
-                display.drawString(0, 0, "Run HTTP POST");
-                display.display();
-
+            display.clear();
+            //text display tests
+            display.flipScreenVertically();
+            display.setFont(ArialMT_Plain_10);
+            display.drawString(0, 0, "Run HTTP POST");
+            display.display();
 
             // file found at server
             if(httpCode == HTTP_CODE_OK) {
+              
                 String payload = http.getString();
                 
                 USE_SERIAL.println(payload + "\n");
-                USE_SERIAL.println("gps data");
-                USE_SERIAL.println(gpsData);
 
-                //text display tests
+                //Display HTTP code
                 display.flipScreenVertically();
-                display.setFont(ArialMT_Plain_16);
+                display.setFont(ArialMT_Plain_10);
                 display.drawString(0, 20, "Result was: " + String(httpCode));
                 display.display();
+                
             }
             
         } else {
             USE_SERIAL.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
         }
-
+        
         http.end();
     }
-
       delay(5000);
-  }
+  }  
+}
+
+String ipToString(IPAddress ip){
+  String s="";
+  for (int i=0; i<4; i++)
+    s += i  ? "." + String(ip[i]) : String(ip[i]);
+  return s;
 }
